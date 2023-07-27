69559d840667fb74e63cc110f3ca9332e40f3040
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-07-27 22:45:23 +0300

    Initial commit

---
+++ src/main/java/com/aleksandrphilimonov/spring/rest/App.java
@@ -0,0 +1,16 @@
+package com.aleksandrphilimonov.spring.rest;
+
+import com.aleksandrphilimonov.spring.rest.configuration.MyConfig;
+import com.aleksandrphilimonov.spring.rest.entity.Employee;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+        Communication communication = context.getBean("communication", Communication.class);
+        List<Employee> allEmployees = communication.getAllEmployees();
+        System.out.println(allEmployees);
+    }
+}
